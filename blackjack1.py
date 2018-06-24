2d994d60b15111d49633c9be673f57329fd3ef64
Andrei Rikkiev <andrei.rikkiev@tut.fi>
2018-06-24 12:42:39 +0300

    third

--- blackjack1.py
+++ blackjack1.py
@@ -69,4 +69,97 @@ class Participant:
         self.total -= self.bet
 
 
+def total_budget(play_round, total):
+    while True:
+        try:
+            if play_round == 0:
+                total = int(input("\nWhat is your budget: "))
+                if total <= 0:
+                    print("Sorry. Budget can't be negative or zero")
+                else:
+                    return total
+            else:
+                print("Your budget is " + str(total))
+                return total
+        except ValueError:
+            print("Please enter an integer!")
+
+
+def take_bet(player):
+    while True:
+        try:
+            bet = int(input("What is your bet: "))
+            if bet <= player.total and bet > 0:
+                player.bet = bet
+                break
+            elif bet == 0:
+                print("Your bet can't be 0")
+            elif bet <= 0:
+                print("Your bet can't be negative")
+            else:
+                print("Your bet can't exceed your total budget of ", player.total)
+        except ValueError:
+            print("Please enter an integer!")
+
+
+def show_some(player, dealer):
+    print("\nPlayer's cards are:")
+    for item in player.cards:
+        print(item)
+    print("\nDealer's cards are:")
+    print('Secret card: value ?\n' + str(dealer.cards[1]))
+
+
+def show_all(player, dealer):
+    print("\nPlayer's cards were:")
+    for item in player.cards:
+        print(item)
+    print("{Player's Hand: " + str(player.value) + "}")
+
+    print("\nDealer's cards were:")
+    for item in dealer.cards:
+        print(item)
+    print("{Dealer's Hand: " + str(dealer.value) + "}")
+
+
+def hit(deck, player_or_dealer):
+    player_or_dealer.add_card(deck.deal(), values)
+    #   possible bug
+    #   if player_or_dealer == player and player.aces > 0:
+    if player_or_dealer.aces > 1:
+        player_or_dealer.adjust_for_ace()
+
+
+def hit_or_stand(deck, player_or_dealer, player, dealer):
+    while True:
+        choice = input("\nWould you prefer to hit or stay? Choose either 'H' or 'S': ").upper()
+
+        if choice == 'H':
+            hit(deck, player_or_dealer)
+            show_some(player, dealer)
+            if player.value >= 21:
+                break
+            # possible bug
+            if player_or_dealer == dealer and (dealer.value >= 17):
+                break
+        elif choice == 'S':
+            break
+        else:
+            print('Please input a valid value')
+
+
+def replay(play_round, total):
+    while True:
+        wantagain = input("\nDo you want to play again? Choose either 'Y' or 'N': ").upper()
+
+        if wantagain == 'Y':
+            print('')
+            main(play_round, total)
+            break
+        elif wantagain == 'N':
+            print('Goodbye. It was a pleasure to play with you!')
+            break
+        else:
+            print('Please input a valid value')
+
 
